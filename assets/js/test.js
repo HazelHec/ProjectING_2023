7ef1f1991f31e17c04dc918c13fb0a12f0bab9c8
= <hazelbre7@gmail.com>
2023-11-19 09:01:31 -0600

    Previo a integracion front

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,21 +1,39 @@
-console.log('hola mundo');
-
+console.log("hola mundo");
+dataEnv = {
+  qo: 1000,
+  pr: 1234,
+  pwf: 342,
+  pb: 123,
+  r: 12
+};
+let response = $.ajax({
+  type: "POST",
+  url: "/productividad/vogel",
+  data: JSON.stringify(dataEnv),
+  contentType: "application/json",
+  dataType: "JSON",
+});
+response.done(function(result) {
+  console.log(result);
+});
+response.fail(function(error){
+  console.log(error);
+});
 
 const btn = document.getElementById("enviar");
-btn.addEventListener("click", function() {
-
+btn.addEventListener("click", function () {
   let dataEnv = {
     qo: parseFloat(document.getElementById("qo").value),
     pr: parseFloat(document.getElementById("pr").value),
     pwf: parseFloat(document.getElementById("pwf").value),
     pb: parseFloat(document.getElementById("pb").value),
-    r: parseInt(document.getElementById("r").value)
+    r: parseInt(document.getElementById("r").value),
   };
-  console.log(typeof(dataEnv));
-  console.log(typeof(dataEnv["qo"]));
+  console.log(typeof dataEnv);
+  console.log(typeof dataEnv["qo"]);
   for (let key in dataEnv) {
-    console.log(typeof(dataEnv[key]));
-  };
+    console.log(typeof dataEnv[key]);
+  }
 
   let request = $.ajax({
     type: "POST",
@@ -27,14 +45,13 @@ btn.addEventListener("click", function() {
   request.done(function (response) {
     console.log(response);
   });
-  
 });
-    // let request = $.ajax({
-    //     type: "post",
-    //     url: "/productividad/vogel",
-    //     data: "data",
-    //     dataType: "JSON",
-    // });
-    // request.done(function(response){
+// let request = $.ajax({
+//     type: "post",
+//     url: "/productividad/vogel",
+//     data: "data",
+//     dataType: "JSON",
+// });
+// request.done(function(response){
 
-    // });
+// });
